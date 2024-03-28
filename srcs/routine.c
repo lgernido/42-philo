d6d93a9ffff85c3aa3b6c61812d17393601901c9
Lucie <luciegernidos.pro@gmail.com>
2024-03-28 18:43:19 +0100

    routine cleaned while structure needs to be properly initiated

--- srcs/routine.c
+++ srcs/routine.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   routine.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lgernido <lgernido@student.42.fr>          +#+  +:+       +#+        */
+/*   By: luciegernidos <luciegernidos@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/28 12:33:42 by lgernido          #+#    #+#             */
-/*   Updated: 2024/03/28 14:12:22 by lgernido         ###   ########.fr       */
+/*   Updated: 2024/03/28 18:42:23 by luciegernid      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,8 @@
 
 void	*daily_routine(void *arg)
 {
-	t_philo	*philosopher;
-	int		position;
-
-	position = *(int *)arg;
-	philosopher = malloc(sizeof(philosopher));
-	if (philosopher == NULL)
-	{
-		printf("Philosopher are not seated\n");
-		return (NULL);
-	}
-	philosopher->position = position;
-	philosopher->meal_ate = 0;
-	printf("Je suis philosophe\nMa position est %d\nJ'ai mange %d repas\n\n",
-		philosopher->position, philosopher->meal_ate);
-	free(philosopher);
-	free(arg);
+	(void)arg;
+	printf("Je suis philosophe\n");
+	// free(arg);
 	return (NULL);
 }
