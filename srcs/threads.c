8f822aff8fb1b066681ef0050558412977cee0e0
Lucie Gernidos <lgernido@paul-f4Ar8s8.clusters.42paris.fr>
2024-03-28 11:53:15 +0100

    no more leaks or segfault, ready to clean the functions and implement a complete routine

--- srcs/threads.c
+++ srcs/threads.c
@@ -6,7 +6,7 @@
 /*   By: lgernido <lgernido@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/27 13:49:58 by lgernido          #+#    #+#             */
-/*   Updated: 2024/03/27 16:15:52 by lgernido         ###   ########.fr       */
+/*   Updated: 2024/03/28 11:33:28 by lgernido         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,13 @@ void	*daily_routine(void *arg)
 	int		position;
 
 	position = *(int *)arg;
-	free(arg);
-	philosopher = malloc(sizeof(philosopher));
+	philosopher = malloc(sizeof(t_philo));
 	philosopher->position = position;
 	philosopher->meal_ate = 0;
-	printf("je suis philosophe\n ma position est %d\nj'ai mange %d repas\n",
+	printf("Je suis philosophe\nMa position est %d\nJ'ai mange %d repas\n\n",
 		philosopher->position, philosopher->meal_ate);
 	free(philosopher);
+	free(arg);
 	return (NULL);
 }
 
@@ -35,22 +35,24 @@ void	init_threads(t_parameters *parameters)
 	int			*safe_value;
 	pthread_t	*thread;
 
-	thread = malloc(sizeof(pthread_t) * parameters->number_of_philosophers);
-	threads_created = 1;
-	safe_value = malloc(sizeof(int));
-	while (threads_created <= parameters->number_of_philosophers)
+	thread = (pthread_t *)malloc(sizeof(*thread)
+			* parameters->number_of_philosophers);
+	threads_created = 0;
+	while (threads_created < parameters->number_of_philosophers)
 	{
-		*safe_value = threads_created;
+		safe_value = (int *)malloc(sizeof(int));
+		*safe_value = threads_created + 1;
 		pthread_create(&thread[threads_created], NULL, &daily_routine,
 			safe_value);
-		printf("thread %d has started\n", threads_created);
+		printf("thread %d has started\n", *safe_value);
 		threads_created++;
 	}
-	threads_executed = 1;
+	threads_executed = 0;
 	while (threads_executed < threads_created)
 	{
 		pthread_join(thread[threads_executed], NULL);
-		printf("thread %d is over\n", threads_executed);
+		printf("thread %d is over\n", threads_executed + 1);
 		threads_executed++;
 	}
+	free(thread);
 }
